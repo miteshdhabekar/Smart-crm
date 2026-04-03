782cd7b338a67dcc2861fb99c2ee5c7529f5e00d
Mitesh Dhabekar <dev0330d@gmail.com>
2026-04-03 10:52:32 +0530

    fourth commit --> deploy backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+app.set("trust proxy", 1);
 const cors = require("cors");
 const sessionMiddleware = require("./config/session");
 const authRoutes = require("./routes/authRoutes");
